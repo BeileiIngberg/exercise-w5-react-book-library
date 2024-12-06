bfac701e80e3873968c0c690c656e578ae5845fa
Beilei Ingberg <beilei.ingberg@gmail.com>
2024-12-06 15:52:52 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import data from "./data.json";
+import Header from "./Header"
+import Book from "./Book"
 
 function App() {
   const { books } = data;
@@ -7,6 +9,10 @@ function App() {
 
   return (
     <>
+    <div>
+            <Header />
+            {}
+        </div>
       <div>Book library goes here</div>
     </>
   );
